27f7ecddb81de0baf10c10d18fce1c1ef8e206bf
VANSH-KAKRAN <vanshkakran82@gmail.com>
2025-01-31 12:22:01 +0530

    deploying

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
-import { StrictMode } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+// import { StrictMode } from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode> 
+// );
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+
+ReactDOM.render(
+  <Router basename="/Assignment">
     <App />
-  </StrictMode> 
+  </Router>,
+  document.getElementById('root')
 );
